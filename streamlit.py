fd4bd92b3a3c009358fa70b414a327976ab1485e
logeshloki585 <Logeshtv21@gmail.com>
2023-06-29 18:51:27 +0530

    final last update

--- streamlit.py
+++ streamlit.py
@@ -7,9 +7,9 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import nltk
 import re
-# nltk.download('wordnet')
-# nltk.download('punkt')
-# nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('stopwords')
 from PIL import Image
 import pytesseract
 
